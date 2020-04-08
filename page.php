ea5c82db1835fdfe02b11f334b21f730800971b3
mikeyrayvon <mikey@interglobal.vision>
2020-04-08 15:20:20 +0200

    change newsletter form text

--- page.php
+++ page.php
@@ -23,12 +23,12 @@ if (have_posts()) {
         <?php
           if ($post->post_name === 'acercade') {
         ?>
-        <div class="grid-item no-gutter item-s-12 offset-m-1 item-m-6 margin-bottom-basic font-size-mid">
+        <div class="grid-item no-gutter item-s-12 offset-m-1 item-m-6 margin-bottom-basic">
         <?php
           if (!empty($options['contact_email'])) {
         ?>
           <div class="margin-bottom-small grid-row">
-            <div class="grid-item">
+            <div class="grid-item font-size-mid">
               <a href="mailto:<?php echo $options['contact_email']; ?>"><?php echo $options['contact_email']; ?></a>
             </div>
           </div>
@@ -37,12 +37,12 @@ if (have_posts()) {
           if (!empty($options['mailchimp_url'])) {
         ?>
           <form id="mailchimp-form" class="grid-row margin-bottom-small align-items-center" novalidate="true">
-            <div class="grid-item item-s-12 margin-bottom-micro">Suscríbete</div>
+            <div class="grid-item item-s-12 margin-bottom-micro">Suscríbete a nuestro boletín</div>
             <div class="grid-item item-s-8 item-m-6">
               <input id="mailchimp-email" type="email" placeholder="Email" name="EMAIL" class="item-s-5 item-m-3">
             </div>
             <div>
-              <button id="mailchimp-submit" type="submit">Subscribe</button>
+              <button id="mailchimp-submit" type="submit">Suscríbete</button>
             </div>
             <div id="mailchimp-response" class="grid-item item-s-12 item-m-10 margin-top-micro">&nbsp;</div>
           </form>
